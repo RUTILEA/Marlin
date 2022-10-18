a9799753910d4a8c8f2d7e652f8063ff1949d12c
abe yoshimasa <yosimasa98pen@gmail.com>
2022-10-18 19:07:48 +0900

    納品コード

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1135,7 +1135,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 80, 80 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100/3, 80, 80, 80 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
